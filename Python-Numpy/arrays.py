5c39b0cf17afe4a802001427b0b9f5ee058e92b8
Haseeb Tariq <chaudhryhaseeb4116@gmail.com>
2026-06-28 14:45:00 +0500

    Numpy Basics Completed

--- Python-Numpy/arrays.py
+++ Python-Numpy/arrays.py
@@ -61,4 +61,100 @@ print(h)
 
 print(h[:, 1 , :]) 
 
+## Initializing different types of array ##
+## All zeros ##
+a = np.zeros(5)
+print(a)
+a = np.zeros((2,3))
+print(a)
+a= np.zeros(((2,3,3)))
+print(a)
+
+## All 1s matrix ##
+b = np.ones((2,3))
+print(b)
+
+## Any other number ##
+c = np.full((2,3), 77)
+print(c)
+
+## Random decimal numbers ##
+d = np.random.rand(4,2)
+print(d)
+
+## Random integer numbers ##
+e = np.random.randint(7, size=(3,2))
+print(e)
+
+## Repeat an array ##
+arr = np.array([[1,2,3]])
+r1 = np.repeat(arr,3,axis = 0)
+print(r1)
+
+print("PRACTICE QUESTION")
+
+output = np.ones([5,5])
+print(output)
+
+z = np.zeros([3,3])
+print(z)
+z[1,1] = 9
+print(z)
+
+output[1:4,1:4] = z
+print(output)
+
+
+## MATHEMATICS ##
+print("MATHEMATICS")
+
+a = np.array([1,2,3,4])
+print(a+2)
+print(a*2)
+print(a-2)
+print(a/2)
+print(a**2)
+print(np.sin(a))
+
+
+
+b = np.array([5,6,7,8])
+print(a+b)
+
+
+## Linear Algebra ##
+print("LINEAR ALGEBRA")
+a = np.ones((2,3))
+b = np.full((3,2), 2)
+print(np.matmul(a,b))
+## Finding Determinant ##
+print("DETERMINANT")
+c = np.identity(3)
+print(np.linalg.det(c))
+
+
+
+## Statistics ## 
+print("STATISTICS")
+stats = np.array([[1,2,3],[4,5,6]])
+print(np.min(stats))
+print(np.max(stats))
+print(np.sum(stats))
+
+print("REORGANIZING ARRAYS")
+before = np.array([[1,2,3,4],[5,6,7,8]])
+after = before.reshape(8,1)
+print(after)
+
+
+## Vertical stacking ##
+print("Vertical Stacking")
+v1 = np.array([1,2,3,4])
+v2 = np.array([5,6,7,8])
+
+print(np.vstack([v1,v2,v1,v2]))
+
+## Horizontal Stacking ##
+print("Horizontal Stacking")
+print(np.hstack([v1,v2,v1,v2]))
 
